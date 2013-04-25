d2fda57641c778eef3e18e10eb492bb1723ffc11
Michelle Lowe <mick_571@yahoo.com>
2013-04-25 09:57:13 -0700

    Corrected favoritesList.js convertToOrdinal function

--- favoritesList/js/favoritesList.js
+++ favoritesList/js/favoritesList.js
@@ -1,31 +1,41 @@
 function favoritesList(things) {
 
   function convertToOrdinal(num) {
-    if(10 < num && num < 14) {
-      return num + "th";
+    var numStr = num.toString();
+    var last = numStr.charAt(numStr.length-1);
+    if (numStr.length === 1) {
+        switch(num) {
+            case 1:
+                return num + "st";
+                break;
+            case 2:
+                return num + "nd";
+                break;
+            case 3:
+                return num + "rd";
+                break;
+            default:
+              return num + "th";
+        }
     } else {
-      switch(num % 10) {
-        case 1:
-          return num + "st";
-          break;
-        case 2:
-          return num + "nd";
-          break;
-        case 3:
-          return num + "rd";
-          break;
-        default:
+      if (numStr.charAt(numStr.length-2) === "1") {
           return num + "th";
-      }
+      } else if (last === "1") {
+          return num + "st";
+      } else if (last === "2") {
+          return num + "nd";
+      } else if (last === "3") {
+          return num + "rd";
+      } else {
+          return num + "th";
+      }  
     }
-
   }
-
 
   for(var i = 0; i < activities.length; i += 1) {
     console.log("My " + convertToOrdinal(i + 1) + " favorite thing to do is " + activities[i] + ".")
   }
 }
 
-var activities = ["eat chocolate chip cookies", "watch Mickey Mouse", "go running", "play golf"];
+var activities = ["eat chocolate chip cookies", "watch Mickey Mouse", "go running", "play golf", "eat chocolate chip cookies", "watch Mickey Mouse", "go running", "play golf", "eat chocolate chip cookies", "watch Mickey Mouse", "go running", "play golf"];
 favoritesList(activities);
